80a837e8cac2b3899d112aea0de8aa2d8587b3b3
EsWonder <sebastiangranda78@gmail.com>
2024-11-10 18:06:26 -0500

    Changes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// Import Express
 const express = require('express');
 const app = express();
+
+// Set the port
 const PORT = process.env.PORT || 3000;
 
+// Define a start route
 app.get('/', (req, res) => {
-    res.send("Hola Mundo desde Node.js!");
+res.send("Hello World");
 });
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+console.log(`Server listening on port ${PORT}`);
 });
